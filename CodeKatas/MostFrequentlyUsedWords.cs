f787de65899b7bb4838ea8cbb9e64891dbf2122c
Typhae <bradley.hoffman@live.co.uk>
2023-11-17 13:55:42 +0000

    added MostFrequentlyUsedWords kata

---
+++ CodeKatas/MostFrequentlyUsedWords.cs
@@ -0,0 +1,43 @@
+﻿using System.Text.RegularExpressions;
+
+namespace CodeKatas
+{
+    public class MostFrequentlyUsedWords
+    {
+        public static List<string> Top3(string s)
+        {
+            Console.WriteLine(s);
+            var dict = new Dictionary<string, int>();
+            s = Regex.Replace(s, "[^a-zA-Z0-9 ']", " ");
+            s = Regex.Replace(s, "'{2,}", "'");
+
+            var list = s.Split(' ').ToList();
+
+            for (int i = 0; i < list.Count; i++)
+            {
+                var currentWord = list[i].ToLower();
+                if (currentWord == " " || currentWord == "" || currentWord == "'")
+                    continue;
+                else if (dict.ContainsKey(currentWord))
+                {
+                    dict[currentWord] = dict[currentWord] + 1;
+                }
+                else
+                {
+                    dict.Add(currentWord, 1);
+                }
+
+            }
+            var keyList = dict
+                .ToList()
+                .OrderByDescending(x => x.Value)
+                .Select(x => x.Key)
+                .Take(3)
+                .ToList();
+
+
+
+            return keyList;
+        }
+    }
+}
---
+++ KataTests/MostFrequentlyUsedWordsTests.cs
@@ -0,0 +1,33 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeKatas.Tests
+{
+    [TestFixture]
+    public class MostFrequentlyUsedWordsTests
+    {
+        [Test]
+        public void TestMostFrequentlyUsedWords()
+        {
+            Assert.That(MostFrequentlyUsedWords.Top3("a a a  b  c c  d d d d  e e e e e"), Is.EqualTo(new List<string> { "e", "d", "a" }));
+            Assert.That(MostFrequentlyUsedWords.Top3("e e e e DDD ddd DdD: ddd ddd aa aA Aa, bb cc cC e e e"), Is.EqualTo(new List<string> { "e", "ddd", "aa" }));
+            Assert.That(MostFrequentlyUsedWords.Top3("  //wont won't won't "), Is.EqualTo(new List<string> { "won't", "wont" }));
+            Assert.That(MostFrequentlyUsedWords.Top3("  , e   .. "), Is.EqualTo(new List<string> { "e" }));
+            Assert.That(MostFrequentlyUsedWords.Top3("  ...  "), Is.EqualTo(new List<string> { }));
+            Assert.That(MostFrequentlyUsedWords.Top3("  '  "), Is.EqualTo(new List<string> { }));
+            Assert.That(MostFrequentlyUsedWords.Top3("  '''  "), Is.EqualTo(new List<string> { }));
+            Assert.That(MostFrequentlyUsedWords.Top3(
+                string.Join("\n", new string[]{"In a village of La Mancha, the name of which I have no desire to call to",
+                  "mind, there lived not long since one of those gentlemen that keep a lance",
+                  "in the lance-rack, an old buckler, a lean hack, and a greyhound for",
+                  "coursing. An olla of rather more beef than mutton, a salad on most",
+                  "nights, scraps on Saturdays, lentils on Fridays, and a pigeon or so extra",
+                  "on Sundays, made away with three-quarters of his income." })), Is.EqualTo(new List<string> { "a", "of", "on" }));
+        }
+
+    }
+}
