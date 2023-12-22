64915d7654cba95cbc8743469023aac2cfcf9135
Typhae <bradley.hoffman@live.co.uk>
2023-12-22 14:22:02 +0000

    added camelCase kata

---
+++ CodeKatas/BreakCamelCase.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeKatas
+{
+    public static class BreakCamelCase
+    {
+        public static string CamelCase(string str)
+        {
+            for (int i = 0; i < str.Length; i++)
+            {
+                if (Char.IsUpper(str[i]))
+                {
+                    str = str.Insert(i, " ");
+                    i++;
+                }
+            }
+            return str;
+        }
+    }
+}
---
+++ KataTests/BreakCamelCaseTests.cs
@@ -0,0 +1,20 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeKatas.Tests
+{
+    [TestFixture]
+    public class BreakCamelCaseTests
+    {
+        [Test]
+        public void testCamelCase()
+        {
+            Assert.That(BreakCamelCase.CamelCase("camelCasing"), Is.EqualTo("camel Casing"));
+            Assert.That(BreakCamelCase.CamelCase("camelCasingTest"), Is.EqualTo("camel Casing Test"));
+        }
+    }
+}
